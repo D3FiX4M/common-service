82934a4ff70671897fa84fec145d3b360d5c1ab9
d3fix4m <m.fedosenko@neotoria.ru>
2023-09-19 08:29:14 +0300

    add api

--- src/main/java/ru/microservices/common/common_service/config/open_feign/OpenFeignAutoConfig.java
+++ src/main/java/ru/microservices/common/common_service/config/open_feign/OpenFeignAutoConfig.java
@@ -1,11 +1,11 @@
-package ru.microservices.common.common_service.config.openFeign;
+package ru.microservices.common.common_service.config.open_feign;
 
 import feign.Retryer;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.microservices.common.common_service.config.openFeign.retry.RetryerConfig;
-import ru.microservices.common.common_service.config.openFeign.retry.RetryerProperty;
+import ru.microservices.common.common_service.config.open_feign.retry.RetryerConfig;
+import ru.microservices.common.common_service.config.open_feign.retry.RetryerProperty;
 
 @Configuration
 public class OpenFeignAutoConfig {
